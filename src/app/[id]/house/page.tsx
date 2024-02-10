82b9e018c46faefc18c1c9fd38cf7d24de2d9262
maaaashi <doyaseizin.7@gmail.com>
2024-02-10 23:35:52 +0900

    design 調整

--- src/app/[id]/house/page.tsx
+++ src/app/[id]/house/page.tsx
@@ -53,12 +53,15 @@ export default function Page({ params }: { params: Params }) {
 
   return (
     <div className='flex flex-col items-center container mx-auto gap-4'>
-      <div className='card w-full shadow-xl image-full'>
+      <div
+        className='card w-full shadow-xl image-full rounded-none'
+        style={{ borderRadius: '0' }}
+      >
         <figure>
           <img src={house.thumbnail} alt='' />
         </figure>
-        <div className='card-body self-end rounded-none'>
-          <h2 className='card-title'>{house.name}</h2>
+        <div className='card-body self-end'>
+          <h2 className='card-title text-2xl'>{house.name}</h2>
         </div>
       </div>
       <div className='bg-stone-100 rounded-lg p-4 flex flex-col gap-2 w-full'>
@@ -82,44 +85,46 @@ export default function Page({ params }: { params: Params }) {
         </section>
       </div>
 
-      <h3 className='font-bold self-start'>参加者</h3>
-      <div className='overflow-x-auto w-full'>
-        <table className='table'>
-          <thead>
-            <tr>
-              <th></th>
-              <th></th>
-              <th>First Name</th>
-              <th>Job</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user: any, index) => {
-              return (
-                <tr key={index}>
-                  <th>{index + 1}</th>
-                  <td>
-                    <Avatar
-                      size={40}
-                      name={user.id}
-                      variant='beam'
-                      colors={[
-                        '#92A1C6',
-                        '#146A7C',
-                        '#F0AB3D',
-                        '#C271B4',
-                        '#C20D90',
-                      ]}
-                    />
-                  </td>
-                  <td>{user.firstname}</td>
-                  <td>{user.job}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </div>
+      <section className='w-full bg-stone-300 p-4 rounded-lg mb-10'>
+        <h3 className='font-bold self-start'>参加者</h3>
+        <div className='overflow-x-auto w-full'>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th></th>
+                <th></th>
+                <th>First Name</th>
+                <th>Job</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user: any, index) => {
+                return (
+                  <tr key={index}>
+                    <th>{index + 1}</th>
+                    <td>
+                      <Avatar
+                        size={40}
+                        name={user.id}
+                        variant='beam'
+                        colors={[
+                          '#92A1C6',
+                          '#146A7C',
+                          '#F0AB3D',
+                          '#C271B4',
+                          '#C20D90',
+                        ]}
+                      />
+                    </td>
+                    <td>{user.firstname}</td>
+                    <td>{user.job}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      </section>
     </div>
   )
 }
