97ed710d9483116537caebfa513f9bee053aa5c6
jinwatanabe <jin.watanabe.6g@gmail.com>
2024-02-10 13:52:06 +0900

    管理者ログインを作成した

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,20 @@
 export default function Page() {
-  return <div>WebPage</div>
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <section className='rounded-xl shadow-lg p-8 m-4 w-full max-w-md bg-white'>
+        <div className='text-lg mb-4'>管理者ログイン</div>
+        <div className='mb-4'>
+          <label className='block text-sm font-medium text-gray-700'>
+            ユーザーID
+          </label>
+          <input
+            type='text'
+            igo-5
+            className='mt-1 block w-full border-gray-300 shadow-sm input-bordered sm:text-sm input'
+          />
+        </div>
+        <button className='btn btn-primary'>登録</button>
+      </section>
+    </div>
+  )
 }
